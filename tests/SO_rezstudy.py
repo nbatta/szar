242c2be379a327330b8b36d9fcd85a80675342eb
Nicholas Battaglia <nbatta@astro.princeton.edu>
2017-01-27 13:38:24 -0500

    S4 parameters included

--- tests/SO_rezstudy.py
+++ tests/SO_rezstudy.py
@@ -17,7 +17,8 @@ cosmologyName = 'LACosmology' # from ini file
 
 #fileFunc = None
 fileFunc = lambda M,z:"data/"+experimentName+"_m"+str(M)+"z"+str(z)+".txt"
-experimentName = ["SO5m","SO6m","SO7m","SO5m_No270","SO6m_No270","SO7m_No270"]
+experimentName = ["S45m","S46m","S47m"]
+#experimentName = ["SO5m","SO6m","SO7m","SO5m_No270","SO6m_No270","SO7m_No270"]
 
 for ii in xrange(len(experimentName)):
 
