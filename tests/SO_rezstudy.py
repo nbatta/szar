7783a70d5b1207310d286c8101c9deee96f09691
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-01-31 15:45:25 -0500

    api changes

--- tests/SO_rezstudy.py
+++ tests/SO_rezstudy.py
@@ -5,9 +5,10 @@ import numpy as np
 from scipy import special
 import matplotlib.pyplot as plt
 import sys, os, time
-from szlib.szcounts import ClusterCosmology,SZ_Cluster_Model,Halo_MF,dictFromSection,listFromConfig
+from szlib.szcounts import ClusterCosmology,SZ_Cluster_Model,Halo_MF
 
-from orphics.tools.output import Plotter
+
+from orphics.tools.io import Plotter,dictFromSection,listFromConfig
 from ConfigParser import SafeConfigParser 
 
 zz = 0.5
