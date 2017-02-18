624c4f9d5323fb555b4d5ccee9a5fc742b747973
Nicholas Battaglia <nbatta@astro.princeton.edu>
2017-02-18 12:30:21 -0500

    ksz files added

--- tests/makeDerivs.py
+++ tests/makeDerivs.py
@@ -20,7 +20,7 @@ cosmoDict = io.dictFromSection(Config,cosmologyName)
 constDict = io.dictFromSection(Config,'constants')
 clusterDict = io.dictFromSection(Config,clusterParams)
 
-experimentName = "S45m"
+experimentName = "AdvAct"
 
 saveId = experimentName + cosmologyName
 upDict = cosmoDict.copy()
