3b687a38c1a0c664b5ca6f789ecf03f86668d40a
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-24 10:10:26 -0500

    minor

---
+++ bin/subOWL.py
@@ -0,0 +1,25 @@
+import time
+import os
+
+numParams = 12
+#numParams = 1
+numCores = 2*numParams+1
+expList = ['SO-5m-noatm','SO-6m-noatm','SO-7m-noatm']#,'S4-5m','S4-6m','S4-7m']
+#expList = ['AdvAct']
+calList = ['owl2']#,'owl1']
+
+for exp in expList:
+
+    for cal in calList:
+
+
+        massGridName = "data/"+cal+".pkl"
+
+
+        cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: ohhaithere;priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/SZ_filter ; mpirun -hostfile %hostfile% python bin/makeDerivs.py allParams "+exp+" "+cal+" "+massGridName+" \" > output"+str(time.time())+".log  &"
+        #cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: ohhaithere;priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/SZ_filter ; mpirun -hostfile %hostfile% python bin/makeDerivs.py w0 "+exp+" "+cal+" "+massGridName+" \" > output"+str(time.time())+".log  &"
+
+        print cmd
+        os.system(cmd)
+        time.sleep(0.3)
+
---
+++ tests/sampleVarianceTests.py
@@ -0,0 +1,26 @@
+import numpy as np
+from szlib.szcounts import ClusterCosmology,SZ_Cluster_Model,Halo_MF,SampleVariance
+from orphics.tools.io import Plotter,dictFromSection,listFromConfig
+from ConfigParser import SafeConfigParser 
+
+clusterParams = 'LACluster' # from ini file
+cosmologyName = 'LACosmology' # from ini file
+iniFile = "input/params.ini"
+Config = SafeConfigParser()
+Config.optionxform=str
+Config.read(iniFile)
+
+lmax = 3000
+cosmoDict = dictFromSection(Config,cosmologyName)
+constDict = dictFromSection(Config,'constants')
+clusterDict = dictFromSection(Config,clusterParams)
+cc = ClusterCosmology(cosmoDict,constDict,lmax,pickling=True)
+
+mrange = np.arange(14.,15.,0.1)
+zrange = np.arange(0.05,1.0,0.1)
+
+sv = SampleVariance(cc,mrange,zrange)
+hb = sv.haloBias()
+print hb.shape
+
+sv.sample_variance_overNsquared(fsky=0.5,lmax=2000)
