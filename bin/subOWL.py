a62674f927a7939a589c97c3d08d86d2d9f57575
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-26 11:29:39 -0500

    fishers

--- bin/subOWL.py
+++ bin/subOWL.py
@@ -1,12 +1,13 @@
 import time
 import os
 
-#numParams = 12
-numParams = 1
+numParams = 12
+#numParams = 1
 numCores = 2*numParams+1
-expList = ['SO-5m-noatm','SO-6m-noatm','SO-7m-noatm']#,'S4-5m','S4-6m','S4-7m']
+expList = ['S4-5m-noatm']#,'SO-6m','SO-7m','S4-5m','S4-6m','S4-7m']
+#'SO-5m-noatm','SO-6m-noatm','SO-7m-noatm',
 #expList = ['AdvAct']
-calList = ['owl2']#'owl1']
+calList = ['owl2']#,'owl1']
 
 for exp in expList:
 
@@ -15,10 +16,11 @@ for exp in expList:
 
         massGridName = "data/"+cal+".pkl"
 
-        cmd = "mpirun -np "+str(numCores)+" python bin/makeDerivs.py w0 "+exp+" "+cal+" "+massGridName+"  > output"+str(time.time())+".log  &"
+        #cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: ohhaithere;priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/SZ_filter ; mpirun -hostfile %hostfile% python bin/makeWaDeriv.py "+exp+" "+cal+" "+massGridName+" data/forDerivsStep0.1 0.1 \" > output"+str(time.time())+".log  &"
+        #cmd = "mpirun -np "+str(numCores)+" python bin/makeDerivs.py w0 "+exp+" "+cal+" "+massGridName+"  > output"+str(time.time())+".log  &"
         #cmd = "mpirun -np "+str(numCores)+" python bin/makeDerivs.py allParams "+exp+" "+cal+" "+massGridName+"  > output"+str(time.time())+".log  &"
 
-        #cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: ohhaithere;priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/SZ_filter ; mpirun -hostfile %hostfile% python bin/makeDerivs.py allParams "+exp+" "+cal+" "+massGridName+" \" > output"+str(time.time())+".log  &"
+        cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: ohhaithere;priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/SZ_filter ; mpirun -hostfile %hostfile% python bin/makeDerivs.py allParams "+exp+" "+cal+" "+massGridName+" \" > output"+str(time.time())+".log  &"
         #cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: ohhaithere;priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/SZ_filter ; mpirun -hostfile %hostfile% python bin/makeDerivs.py w0 "+exp+" "+cal+" "+massGridName+" \" > output"+str(time.time())+".log  &"
 
         print cmd
