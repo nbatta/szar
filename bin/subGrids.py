4ab7a30c017e8dc3df51ee5ec5f0f2f0dc289212
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-28 19:05:24 -0500

    automation

--- bin/subGrids.py
+++ bin/subGrids.py
@@ -2,15 +2,15 @@ import time
 import os
 
 
-#expList = ['SO-5m','SO-6m','SO-7m','S4-5m','S4-6m','S4-7m']
-expList = ['S4-5m-noatm','S4-6m-noatm','S4-7m-noatm']
+expList = ['SO-3m','SO-5m','SO-6m','SO-7m','S4-3m','S4-5m','S4-6m','S4-7m']
+#expList = ['S4-5m-noatm','S4-6m-noatm','S4-7m-noatm']
 #expList = ['AdvAct']
-calList = ['CMB_pol']#,'CMB_pol']#,'OWL1','OWL2','CMBAllOWL1','CMBAllOWL2','CMBPolOWL1','CMBPolOWL#expList = ['SO-6m','SO-7m','S4-5m','S4-6m','S4-7m']
+calList = ['CMB_all_coarse','CMB_pol_coarse']#,'CMB_pol']#,'OWL1','OWL2','CMBAllOWL1','CMBAllOWL2','CMBPolOWL1','CMBPolOWL#expList = ['SO-6m','SO-7m','S4-5m','S4-6m','S4-7m']
 #calList = ['CMB_all_nodelens','CMB_pol_nodelens','CMB_all','CMB_pol']#,'OWL1','OWL2','CMBAllOWL1','CMBAllOWL2','CMBPolOWL1','CMBPolOWL2']
 #expList = ['S4-7m']
 #calList = ['CMB_all']#,'OWL1','OWL2','CMBAllOWL1','CMBAllOWL2','CMBPolOWL1','CMBPolOWL2']
 
-numCores = 93
+numCores = 31
 
 for exp in expList:
     for cal in calList:
