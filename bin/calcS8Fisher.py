f665d474dff854e256f21c2f6dc3c14192acba95
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-03-14 22:31:31 -0400

    bug fix for new s8 code

---
+++ bin/calcS8Fisher.py
@@ -0,0 +1,177 @@
+import matplotlib
+matplotlib.use('Agg')
+import numpy as np
+import sys, os, time
+from szlib.szcounts import rebinN
+from orphics.tools.io import Plotter,dictFromSection,listFromConfig
+from ConfigParser import SafeConfigParser 
+import cPickle as pickle
+
+
+expName = sys.argv[1]
+gridName = sys.argv[2]
+calName = sys.argv[3]
+
+
+iniFile = "input/pipeline.ini"
+Config = SafeConfigParser()
+Config.optionxform=str
+Config.read(iniFile)
+version = Config.get('general','version')
+pzcutoff = Config.getfloat('general','photoZCutOff')
+
+fparams = {}   # the 
+for (key, val) in Config.items('params'):
+    if ',' in val:
+        param, step = val.split(',')
+        fparams[key] = float(param)
+    else:
+        fparams[key] = float(val)
+
+
+
+bigDataDir = Config.get('general','bigDataDirectory')
+
+saveId = expName + "_" + gridName + "_" + calName + "_v" + version
+
+from orphics.tools.io import dictFromSection, listFromConfig
+fsky = Config.getfloat(expName,'fsky')
+
+# get s/n q-bins
+qs = listFromConfig(Config,'general','qbins')
+qspacing = Config.get('general','qbins_spacing')
+if qspacing=="log":
+    qbins = np.logspace(np.log10(qs[0]),np.log10(qs[1]),int(qs[2])+1)
+elif qspacing=="linear":
+    qbins = np.linspace(qs[0],qs[1],int(qs[2])+1)
+else:
+    raise ValueError
+
+
+
+from orphics.tools.io import listFromConfig
+zs = listFromConfig(Config,gridName,'zrange')
+z_edges = np.arange(zs[0],zs[1]+zs[2],zs[2])
+zrange = (z_edges[1:]+z_edges[:-1])/2.
+
+
+NFid_mzq = np.load(bigDataDir+"N_mzq_"+saveId+"_fid_sigma8.npy")
+NFid_mzq_alt = np.load(bigDataDir+"N_mzq_"+saveId+"_fid.npy")
+assert np.all(NFid_mzq==NFid_mzq_alt)
+
+
+    
+import itertools
+new_z_edges, N_fid = rebinN(NFid_mzq,pzcutoff,z_edges)
+N_fid = N_fid*fsky
+print "Total number of clusters: ", N_fid.sum() #getTotN(N_fid,mgrid,zgrid,qbins)
+
+
+
+# Fisher params
+fishSection = 'fisher-lcdm'
+zlist = ["S8Z"+str(i) for i in range(len(zrange))]
+origParams = Config.get(fishSection,'paramList').split(',')
+paramList = origParams+zlist
+saveName = Config.get(fishSection,'saveSuffix')
+numParams = len(paramList)
+Fisher = np.zeros((numParams,numParams))
+paramCombs = itertools.combinations_with_replacement(paramList,2)
+
+
+# sId = expName + "_" + gridName 
+# sovernsquareEach = np.loadtxt(bigDataDir+"sampleVarGrid_"+sId+".txt")
+# sovernsquare =  np.dstack([sovernsquareEach]*len(qbins))
+
+
+# Populate Fisher
+for param1,param2 in paramCombs:
+    if param1=='tau' or param2=='tau': continue
+    
+    new_z_edges, dN1 = rebinN(np.load(bigDataDir+"dNdp_mzq_"+saveId+"_"+param1+".npy"),pzcutoff,z_edges)
+    new_z_edges, dN2 = rebinN(np.load(bigDataDir+"dNdp_mzq_"+saveId+"_"+param2+".npy"),pzcutoff,z_edges)
+    dN1 = dN1[:,:,:]*fsky
+    dN2 = dN2[:,:,:]*fsky
+
+    
+    # Nup = np.load(bigDataDir+"Nup_mzq_"+saveId+"_"+param1+".npy")*fsky
+    # Ndn = np.load(bigDataDir+"Ndn_mzq_"+saveId+"_"+param2+".npy")*fsky
+    # print param1,Nup.sum()
+    # print param1,Ndn.sum()
+
+
+    i = paramList.index(param1)
+    j = paramList.index(param2)
+
+    assert not(np.any(np.isnan(dN1)))
+    assert not(np.any(np.isnan(dN2)))
+    assert not(np.any(np.isnan(N_fid)))
+
+
+    with np.errstate(divide='ignore'):
+        FellBlock = dN1*dN2*np.nan_to_num(1./(N_fid))#+(N_fid*N_fid*sovernsquare)))
+
+    Fell = FellBlock.sum()
+        
+       
+    Fisher[i,j] = Fell
+    Fisher[j,i] = Fell    
+
+
+
+# Planck and BAO Fishers
+planckFile = Config.get(fishSection,'planckFile')
+try:
+    baoFile = Config.get(fishSection,'baoFile')
+except:
+    baoFile = ''
+
+# Number of non-SZ params (params that will be in Planck/BAO)
+numCosmo = Config.getint(fishSection,'numCosmo')
+numLeft = len(paramList) - numCosmo
+
+fisherPlanck = 0.
+if planckFile!='':
+    try:
+        fisherPlanck = np.loadtxt(planckFile)
+    except:
+        fisherPlanck = np.loadtxt(planckFile,delimiter=',')
+    fisherPlanck = np.pad(fisherPlanck,pad_width=((0,numLeft),(0,numLeft)),mode="constant",constant_values=0.)
+
+if baoFile!='':
+    raise NotImplementedError
+
+
+
+# pl = Plotter()
+# pl.plot2d(cov2corr(fisherPlanck))
+# pl.done("output/fisherplanck.png")
+# sys.exit()
+
+
+f = Fisher+fisherPlanck #[11:,11:]#+fisherPlanck
+
+
+
+
+inv = np.linalg.inv(f)
+
+err = np.sqrt(np.diagonal(inv))[len(origParams):]
+
+print err
+
+outDir = "/gpfs01/astro/www/msyriac/"
+
+
+pl = Plotter(labelX="$z$",labelY="Error on $\sigma_8(z)/\sigma_8(0)$")
+pl.addErr(zrange,zrange*0.+1.,yerr=err)
+pl.legendOn()
+pl._ax.set_ylim(0.9,1.1)
+pl.done(outDir+"s8errsowl.png")
+
+
+from orphics.tools.stats import cov2corr
+corr = cov2corr(f)    
+pl = Plotter()
+pl.plot2d(corr)
+pl.done(outDir+"corrowl.png")
