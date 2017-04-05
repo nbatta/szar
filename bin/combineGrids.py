29c053b36b182aae07b337dfaff2d12eb4081ac3
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-04-05 16:40:20 -0400

    new HSC grid

--- bin/combineGrids.py
+++ bin/combineGrids.py
@@ -1,12 +1,14 @@
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import sys
+import sys,os
 import cPickle as pickle
 import numpy as np
 
-gridList = sys.argv[1:-1]
+gridList = sys.argv[1:]
 outName = sys.argv[-1]
+
+outDir = os.environ['WWW']
 
 print gridList
 print outName
@@ -21,19 +23,27 @@ zmaxes = []
 dms = []
 dzs = []
 
+mindicesList = [15,20,25,30]
+
 from orphics.tools.output import Plotter
-#pl = Plotter(labelX="$z$",labelY="S/N per cluster",ftsize=14)
+pl = Plotter(labelX="$z$",labelY="S/N per cluster",ftsize=14)
 
-for gridFile,ls,lab in zip(gridList,['-','--'],['CMB lensing','optical lensing']):
+for gridFile,ls,lab,outPlot in zip(gridList,['-','--'],['CMB lensing','optical lensing'],['cmb','owl']):
 
-    filen = "data/"+gridFile+".pkl"
+    filen = gridFile #"data/"+gridFile+".pkl"
     
-    mexpgrid,zgrid,errgrid = pickle.load(open(filen,'rb'))
+    medges,zedges,errgrid = pickle.load(open(filen,'rb'))
+    M_edges = 10**medges
+    M = (M_edges[1:]+M_edges[:-1])/2.
+    mexpgrid = np.log10(M)
+    zgrid = (zedges[1:]+zedges[:-1])/2.
 
     mmin = mexpgrid[0]
     mmax = mexpgrid[-1]
     zmin = zgrid[0]
     zmax = zgrid[-1]
+
+    print mmin,mmax,zmin,zmax
 
     grids[gridFile] = (mexpgrid,zgrid,errgrid)
 
@@ -45,18 +55,29 @@ for gridFile,ls,lab in zip(gridList,['-','--'],['CMB lensing','optical lensing']
     dzs.append(min(np.diff(zgrid)))
 
     sngrid = 1./errgrid
+    print sngrid.shape
     
-    pgrid = np.rot90(sngrid)
-    pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
-    pl.plot2d(pgrid,extent=[mmin,mmax,zmin,zmax],levels=[3.0,5.0],labsize=14,aspect="auto")
-    pl.done("output/"+gridFile+".png")
+    # pgrid = np.rot90(sngrid)
+    # pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
+    # pl.plot2d(pgrid,extent=[mmin,mmax,zmin,zmax],levels=[3.0,5.0],labsize=14,aspect="auto")
+    # pl.done(outDir+outPlot+".png")
 
-    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.0)),:].ravel(),ls=ls,label=lab+" 10^14 Msol/h")
-    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.3)),:].ravel(),ls=ls,label=lab+" 10^14.3 Msol/h")
-    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.5)),:].ravel(),ls=ls,label=lab+" 10^14.5 Msol/h")
-    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.7)),:].ravel(),ls=ls,label=lab+" 10^14.7 Msol/h")
-    # plt.gca().set_color_cycle(None)
+    rtol = 1.e-3
+    #pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.0,rtol=rtol)),:].ravel(),ls=ls,label=lab+" 10^14 Msol/h")
+    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.3,rtol=rtol)),:].ravel(),ls=ls,label=lab+" 10^14.3 Msol/h")
+    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.5,rtol=rtol)),:].ravel(),ls=ls,label=lab+" 10^14.5 Msol/h")
+    # pl.add(zgrid,sngrid[np.where(np.isclose(mexpgrid,14.7,rtol=rtol)),:].ravel(),ls=ls,label=lab+" 10^14.7 Msol/h")
 
+    for ind in mindicesList:
+        pl.add(zgrid,sngrid[ind,:].ravel(),ls=ls,label=lab+" 10^"+str(mexpgrid[ind])+" Msol/h")
+        print mexpgrid[ind]
+
+    
+    plt.gca().set_color_cycle(None)
+
+pl.legendOn(loc='upper right',labsize=8)
+pl.done(outDir+"slice.pdf")
+    
 sys.exit()
 
 outmgrid = np.arange(min(mmins),max(mmaxes),min(dms))
@@ -78,7 +99,7 @@ for key in grids:
     # pgrid = np.rot90(1./outerrgrid)
     # pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
     # pl.plot2d(pgrid,extent=[mmin,mmax,zmin,zmax],levels=[1.0,3.0,5.0],labsize=14)
-    # pl.done("output/"+key+".png")
+    # pl.done(outDir+key+".png")
 
 
     jointgridsqinv += (1./outerrgrid**2.)
@@ -94,14 +115,14 @@ jointgrid = np.sqrt(1./jointgridsqinv)
 # pl.add(outzgrid,sngrid[np.where(np.isclose(outmgrid,14.7)),:].ravel(),ls=ls,label=lab+" 10^14.7 Msol/h")
 
 pl.legendOn(loc='upper right',labsize=8)
-pl.done("output/slice.pdf")
+pl.done(outDir+"slice.pdf")
 
 
 from orphics.tools.output import Plotter
 pgrid = np.rot90(1./jointgrid)
 pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
 pl.plot2d(pgrid,extent=[mmin,mmax,zmin,zmax],levels=[1.0,3.0,5.0],labsize=14)
-pl.done("output/joint.png")
+pl.done(outDir+"joint.png")
 
 # tmgrid = np.arange(mmin,mmax,0.5)
 # tzgrid = np.arange(zmin,zmax,0.5)
@@ -111,7 +132,7 @@ pl.done("output/joint.png")
 # pgrid = np.rot90(1./coarsegrid)
 # pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
 # pl.plot2d(pgrid,extent=[mmin,mmax,zmin,zmax],levels=[1.0,3.0,5.0],labsize=14)
-# pl.done("output/coarse.png")
+# pl.done(outDir+"coarse.png")
 
 
 # pickle.dump((tmgrid,tzgrid,coarsegrid),open("data/testGrid.pkl",'wb'))
---
+++ scripts/unpackHSC.py
@@ -0,0 +1,58 @@
+import numpy as np
+from orphics.tools.io import dictFromSection, listFromConfig, Plotter
+from orphics.analysis.flatMaps import interpolateGrid
+import sys, os
+from ConfigParser import SafeConfigParser 
+import cPickle as pickle
+
+gridName = "grid-default"
+outDir = os.environ['WWW']
+
+iniFile = "input/pipeline.ini"
+Config = SafeConfigParser()
+Config.optionxform=str
+Config.read(iniFile)
+
+bigDataDir = Config.get('general','bigDataDirectory')
+
+ms = listFromConfig(Config,gridName,'mexprange')
+Mexp_edges = np.arange(ms[0],ms[1]+ms[2],ms[2])
+zs = listFromConfig(Config,gridName,'zrange')
+z_edges = np.arange(zs[0],zs[1]+zs[2],zs[2])
+
+
+M_edges = 10**Mexp_edges
+M = (M_edges[1:]+M_edges[:-1])/2.
+mgrid = np.log10(M)
+
+zgrid = (z_edges[1:]+z_edges[:-1])/2.
+
+
+zz = np.arange(0.1,2.01,0.05)
+MMexp = np.arange(13.5,15.71,0.1)
+MM = 10**MMexp
+
+hscgrid = np.loadtxt("data/HSC_DeltalnM_z0_z2_17_04_04.txt")
+
+
+sngrid = 1./hscgrid    
+pgrid = np.rot90(sngrid)
+pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
+pl.plot2d(pgrid,extent=[MMexp.min(),MMexp.max(),zz.min(),zz.max()],levels=[3.0,5.0],labsize=14,aspect="auto")
+pl.done(outDir+"origHSCgrid.png")
+
+
+print hscgrid.shape
+
+
+#outmerr = interpolateGrid(hscgrid,MM,zz,M,zgrid,regular=False,kind="cubic",bounds_error=False,fill_value=np.inf)
+outmerr = interpolateGrid(hscgrid,MM,zz,M,zgrid,regular=False,kind="cubic",bounds_error=False)
+
+
+sngrid = 1./outmerr
+pgrid = np.rot90(sngrid)
+pl = Plotter(labelX="$\\mathrm{log}_{10}(M)$",labelY="$z$",ftsize=14)
+pl.plot2d(pgrid,extent=[mgrid.min(),mgrid.max(),zgrid.min(),zgrid.max()],levels=[3.0,5.0],labsize=14,aspect="auto")
+pl.done(outDir+"interpHSCgrid.png")
+
+pickle.dump((Mexp_edges,z_edges,outmerr),open(bigDataDir+"lensgrid_"+gridName+"_owl.pkl",'wb'))
