63f52e27a61701eb2e552ea6769e1032df76142f
Nicholas Battaglia <nbatta@astro.princeton.edu>
2017-06-08 16:04:32 -0400

    main ILC initialization almost ready

--- szar/ilc.py
+++ szar/ilc.py
@@ -4,12 +4,13 @@ from scipy.interpolate import interp1d
 from orphics.tools.cmb import noise_func
 from szar.foregrounds import fgNoises
 from szar.counts import f_nu
+from orphics.tools.io import Plotter
 import numpy.matlib
 
 class ILC_simple:
     def __init__(self,clusterCosmology, \
                  fwhms=[1.5],rms_noises =[1.], freqs = [150.],lmax=8000,lknee=0.,alpha=1., \
-                 dell=1000.,ksz_file='input/ksz_BBPS.txt',ksz_p_file='input/ksz_p_BBPS.txt', \
+                 dell=1.,ksz_file='input/ksz_BBPS.txt',ksz_p_file='input/ksz_p_BBPS.txt', \
                  tsz_cib_file='input/sz_x_cib_template.dat' fg=True):
 
         self.cc = clusterCosmology
@@ -21,7 +22,7 @@ class ILC_simple:
             fq_mat   = freqs
             fq_mat_t = freqs
 
-        fgs = fgNoises(self.cc.c,ksz_file=ksz_file,ksz_p_file=ksz_p_file,tsz_cib_file=tsz_cib_file)
+        self.fgs = fgNoises(self.cc.c,ksz_file=ksz_file,ksz_p_file=ksz_p_file,tsz_cib_file=tsz_cib_file)
 
         self.dell = dell
         self.evalells = np.arange(2,lmax,self.dell)
@@ -30,8 +31,6 @@ class ILC_simple:
         self.W_ll_tsz = np.array(len(self.evalells),len(freqs))
         self.W_ll_cmb = np.array(len(self.evalells),len(freqs))
         self.f_nu_arr = np.array(freqs)*0.0
-
-        b_0 = 1.
 
         for ii in xrange(len(freqs)):
             self.f_nu_arr[ii] = f_nu(self.cc.c,freqs[ii])
@@ -44,13 +43,13 @@ class ILC_simple:
         
             nells = np.outer(inst_noise,inst_noise)
 
-            totfg = ((fgs.rad_ps(self.evalells[ii],fq_mat,fq_mat_t) + fgs.cib_p(self.evalells[ii],fq_mat,fq_mat_t) +
-                      fgs.cib_c(self.evalells[ii],fq_mat,fq_mat_t) + fgs.tSZ_CIB(self.evalells[ii],fq_mat,fq_mat_t))
+            totfg = ((self.fgs.rad_ps(self.evalells[ii],fq_mat,fq_mat_t) + self.fgs.cib_p(self.evalells[ii],fq_mat,fq_mat_t) +
+                      self.fgs.cib_c(self.evalells[ii],fq_mat,fq_mat_t) + self.fgs.tSZ_CIB(self.evalells[ii],fq_mat,fq_mat_t))
                       / self.cc.c['TCMBmuK']**2. / ((self.evalells[ii]+1.)*self.evalells[ii]) * 2.* np.pi )
 
-            ksz = fq_mat*0.0 + fgs.ksz_temp(self.evalells[ii])) / self.cc.c['TCMBmuK']**2. / ((self.evalells[ii]+1.)*self.evalells[ii]) * 2.* np.pi
+            ksz = fq_mat*0.0 + self.fgs.ksz_temp(self.evalells[ii])) / self.cc.c['TCMBmuK']**2. / ((self.evalells[ii]+1.)*self.evalells[ii]) * 2.* np.pi
 
-            tsz = fgs.tSZ(self.evalells[ii],fq_mat,fq_mat_t) / self.cc.c['TCMBmuK']**2. / ((self.evalells[ii]+1.)*self.evalells[ii]) * 2.* np.pi            
+            tsz = self.fgs.tSZ(self.evalells[ii],fq_mat,fq_mat_t) / self.cc.c['TCMBmuK']**2. / ((self.evalells[ii]+1.)*self.evalells[ii]) * 2.* np.pi            
 
             N_ll_for_tsz = nells + totfg + cmb_els + ksz 
             N_ll_for_cmb = nells + totfg + tsz
@@ -61,10 +60,42 @@ class ILC_simple:
             N_ll_for_tsz_inv = np.linalg.inv(N_ll_for_tsz)
             N_ll_for_cmb_inv = np.linalg.inv(N_ll_for_cmb)
 
-            self.W_ll_tsz[ii,:] = b_0 * 1./np.dot(np.transpose(f_nu_arr),np.dot(N_ll_for_tsz_inv,f_nu_arr)) \
+            self.W_ll_tsz[ii,:] = 1./np.dot(np.transpose(f_nu_arr),np.dot(N_ll_for_tsz_inv,f_nu_arr)) \
                                   * np.dot(np.transpose(f_nu_tsz),N_ll_for_tsz_inv)
-            self.W_ll_cmb[ii,:] = b_0 * 1./np.dot(np.transpose(f_nu_cmb),np.dot(N_ll_for_cmb_inv,f_nu_cmb)) \
+            self.W_ll_cmb[ii,:] = 1./np.dot(np.transpose(f_nu_cmb),np.dot(N_ll_for_cmb_inv,f_nu_cmb)) \
                                   * np.dot(np.transpose(f_nu_cmb),N_ll_for_cmb_inv)
 
             self.N_ll_tsz[ii] = np.dot(np.transpose(self.W_ll_tsz[ii,:]),np.dot(N_ll_for_tsz,self.W_ll_tsz[ii,:]))
             self.N_ll_cmb[ii] = np.dot(np.transpose(self.W_ll_cmb[ii,:]),np.dot(N_ll_for_cmb,self.W_ll_cmb[ii,:]))
+
+    def Forecast_Cellyy(self,els):
+        
+        cls = self.fgs.tSZ(self.evalells,freqs[0],freqs[0]) / self.cc.c['TCMBmuK']**2. \
+             / ((self.evalells+1.)*self.evalells) * 2.* np.pi
+
+
+        return cls,errs,sn2
+
+    def Cellcmb(self):
+
+        els_int = np.arange(2,lmax,1.)
+
+
+
+        els = 1.
+        cls = 1.
+        errs = 1.
+
+        return els,cls,errs,sn2
+
+    def PlotyWeights(self):
+        
+        self.W_ll_cmb[:,]
+
+        #plot
+        pl = Plotter()
+        pl._ax.set_ylim(-3.6,-0.8)
+        pl.add(np.log10(M),np.log10(N[:,0]*M/(self.cc.rhoc0om)),color='black')
+        pl.add(np.log10(M),np.log10(N_8[:,0]*M/(self.cc.rhoc0om)),color='black')
+        pl.add(np.log10(M),np.log10(N_32[:,0]*M/(self.cc.rhoc0om)),color='black')
+        pl.done("output/tinkervals.png")
