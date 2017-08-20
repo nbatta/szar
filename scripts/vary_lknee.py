a2bc1c84a3bb2825b9192a949ad28c32acd519f7
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-08-20 12:38:06 -0400

    cmb fisher pickling

---
+++ scripts/vary_lknee.py
@@ -0,0 +1,31 @@
+import time
+import os
+import numpy as np
+
+expList = ['S4-1.5-paper','S4-2.0-paper','S4-2.5-paper','S4-3.0-paper']
+
+
+numCores = 50
+
+gridName = "grid-default"
+
+comp = "gen6"
+
+
+lkneeList = np.arange(0,6000,500)
+alphaList = [-4.,-4.5,-5.]
+
+for exp in expList:
+
+    for lknee in lkneeList:
+        for alpha in alphaList:
+
+
+            # do only sz
+            cmd = "nohup wq sub -r \"mode:bycore;N:"+str(numCores)+";hostfile: auto;job_name: "+exp+";priority:med\" -c \"source ~/.bash_profile ; source ~/.bashrc ; cd ~/repos/szar ; mpirun -hostfile %hostfile% python bin/makeGrid.py "+exp+" "+gridName+" --skip-lensing -l "+str(lknee)+" -a "+str(alpha)+" \" > output"+str(time.time())+"_szgrid_"+exp+".log  &"
+
+
+            print cmd
+            os.system(cmd)
+            time.sleep(0.3)
+
