4a934f218999fbd89759165787dd73eb7b0950d3
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-08-22 17:15:26 -0400

    cmb fisher pickling

--- paper/FigPrior.py
+++ paper/FigPrior.py
@@ -9,6 +9,7 @@ from orphics.tools.io import Plotter
 import matplotlib.pyplot as plt
 from szar.fisher import getFisher
 from szar.counts import rebinN
+import szar.fisher as sfisher
 
 
 expName = sys.argv[1]
@@ -21,19 +22,31 @@ Config = SafeConfigParser()
 Config.optionxform=str
 Config.read(iniFile)
 
+bigDataDir = Config.get('general','bigDataDirectory')
 
 fishSection = 'fisher-'+fishName
 paramList = Config.get(fishSection,'paramList').split(',')
 paramLatexList = Config.get(fishSection,'paramLatexList').split(',')
 saveName = Config.get(fishSection,'saveSuffix')
+version = Config.get('general','version') 
+pzcutoff = Config.getfloat('general','photoZCutOff')
+fsky = Config.getfloat(expName,'fsky')
 
+zs = listFromConfig(Config,gridName,'zrange')
+z_edges = np.arange(zs[0],zs[1]+zs[2],zs[2])
 
-FisherTot, paramList = sfisher.cluster_fisher_from_config(Config,expName,gridName,calName,fishName)
+saveId = sfisher.save_id(expName,gridName,calName,version)
+derivRoot = sfisher.deriv_root(bigDataDir,saveId)
+# Fiducial number counts
+new_z_edges, N_fid = sfisher.rebinN(np.load(sfisher.fid_file(bigDataDir,saveId)),pzcutoff,z_edges)
+N_fid = N_fid*fsky
+print "Effective number of clusters: ", N_fid.sum()
+
 
 
 from collections import OrderedDict
 priorList = OrderedDict()
-priorList['tau'] = 0.1
+priorList['tau'] = 0.01
 priorList['H0'] = 10.0
 priorList['omch2'] = 0.002
 priorList['ombh2'] = 0.00023
@@ -55,7 +68,9 @@ elif 'w0' in fishName:
     pl = Plotter(labelY="$\\frac{\sigma("+paramLatexList[paramList.index("w0")]+")}{"+paramLatexList[paramList.index("w0")]+"}\%$",labelX="Iteration",ftsize=12)
 
 
-for doBAO in [False,True]:    
+#for doBAO in [False,True]:    
+for fishSection in ["fisher-"+fishName,"fisher-"+fishName+"-DESI"]:
+
 
     priorNameList = []
     priorValueList = []
@@ -86,24 +101,87 @@ for doBAO in [False,True]:
 
             ##########################
             # Populate Fisher
-            Fisher = getFisher(N_fid,paramList,priorNameList,priorValueList,bigDataDir,saveId,pzcutoff,z_edges,fsky)
+            Fisher = sfisher.getFisher(N_fid,paramList,priorNameList,priorValueList,derivRoot,pzcutoff,z_edges,fsky)
             ##########################
 
+            # Number of non-SZ params (params that will be in Planck/BAO)
+            numCosmo = Config.getint(fishSection,'numCosmo')
+            numLeft = len(paramList) - numCosmo
+
+            try:
+                otherFishers = Config.get(fishSection,'otherFishers').split(',')
+            except:
+                traceback.print_exc()
+                print "No other fishers found."
+                otherFishers = []
+            for otherFisherFile in otherFishers:
+                try:
+                    other_fisher = np.loadtxt(otherFisherFile)
+                except:
+                    other_fisher = np.loadtxt(otherFisherFile,delimiter=',')
+                other_fisher = sfisher.pad_fisher(other_fisher,numLeft)
+                Fisher += other_fisher
 
             
-            fisherBAO = Fisher.copy()*0.
-            if doBAO:
-                if baoFile!='':
+
+
+            try:
+                do_cmb_fisher = Config.getboolean(fishSection,"do_cmb_fisher")
+            except:
+                do_cmb_fisher = False
+
+            try:
+                do_clkk_fisher = Config.getboolean(fishSection,"do_clkk_fisher")
+            except:
+                do_clkk_fisher = False
+
+
+            if do_clkk_fisher:
+                assert do_cmb_fisher, "Sorry, currently Clkk fisher requires CMB fisher to be True as well."
+                lensName = Config.get(fishSection,"clkk_section")
+            else:
+                lensName = None
+
+            if do_cmb_fisher:
+
+
+                import pyfisher.clFisher as pyfish
+                # Load fiducials and derivatives
+                cmbDerivRoot = Config.get("general","cmbDerivRoot")
+                cmbParamList = paramList[:numCosmo]
+
+
+                cmb_fisher_loaded = False
+                if True:
+                    import time
+                    hashval = sfisher.hash_func(cmbParamList,expName,lensName,do_clkk_fisher,time.strftime('%Y%m%d'))
+                    pkl_file = "output/pickledFisher_"+hashval+".pkl"
+
                     try:
-                        fisherBAO = np.loadtxt(baoFile)
+                        cmb_fisher = pickle.load(open(pkl_file,'rb'))
+                        cmb_fisher_loaded = True
+                        print "Loaded pickled CMB fisher."
                     except:
-                        fisherBAO = np.loadtxt(baoFile,delimiter=',') 
-                    fisherBAO = np.pad(fisherBAO[:numCosmo,:numCosmo],pad_width=((0,numLeft),(0,numLeft)),mode="constant",constant_values=0.) # !!!!
-                        
+                        pass
 
-            FisherTot = Fisher + fisherPlanck
-            FisherTot += fisherBAO
+                if not(cmb_fisher_loaded):
+                    fidCls = np.loadtxt(cmbDerivRoot+'_fCls.csv',delimiter=',')
+                    dCls = {}
+                    for paramName in cmbParamList:
+                        dCls[paramName] = np.loadtxt(cmbDerivRoot+'_dCls_'+paramName+'.csv',delimiter=',')
 
+                    print "Calculating CMB fisher matrix..."
+                    cmb_fisher = pyfish.fisher_from_config(fidCls,dCls,cmbParamList,Config,expName,lensName)
+                    if True:
+                        print "Pickling CMB fisher..."
+                        pickle.dump(cmb_fisher,open(pkl_file,'wb'))
+
+
+                cmb_fisher = sfisher.pad_fisher(cmb_fisher,numLeft)
+            else:
+                cmb_fisher = 0.    
+
+            FisherTot = Fisher+cmb_fisher
 
             Finv = np.linalg.inv(FisherTot)
 
@@ -125,7 +203,7 @@ for doBAO in [False,True]:
             k+=1
 
         priorLabel = paramLatexList[paramList.index(prior)]
-        if doBAO:
+        if "DESI" in fishSection:
             lab = None
             lss = "--"
         else:
