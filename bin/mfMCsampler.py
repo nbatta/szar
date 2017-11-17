d00909fb1d84b29739e24ee8afaac80b75f9b828
Nicholas Battaglia <nbatta@astro.princeton.edu>
2017-11-17 18:08:23 -0500

    added scaling relation test to mf sampler

--- bin/mfMCsampler.py
+++ bin/mfMCsampler.py
@@ -1,5 +1,6 @@
 import numpy as np
 from szar.counts import ClusterCosmology,Halo_MF
+from szar.szproperties import SZ_Cluster_Model
 from orphics.tools.io import dictFromSection, listFromConfig
 from ConfigParser import SafeConfigParser
 import cPickle as pickle
@@ -16,6 +17,7 @@ Config.read(iniFile)
 bigDataDir = Config.get('general','bigDataDirectory')
 clttfile = Config.get('general','clttfile')
 constDict = dictFromSection(Config,'constants')
+clusterDict = dictFromSection(Config,'cluster_params')
 version = Config.get('general','version')
 expName = "S4-1.0-CDT"
 gridName = "grid-owl2"
@@ -45,8 +47,33 @@ elapsed1 = (time.clock() - start)
 print elapsed1
 print len(samples)
 
-ids = np.random.randint(len(samples)/2.,size=1000)
+nclust = 1000
+ids = np.random.randint(len(samples)/2.,size=nclust)
 
-plt.plot(samples[:,0],samples[:,1],'x')
-plt.plot(samples[ids,0],samples[ids,1],'o')
-plt.savefig('default2.png', bbox_inches='tight',format='png') 
+#plt.plot(samples[:,0],samples[:,1],'x')
+#plt.plot(samples[ids,0],samples[ids,1],'o')
+#plt.savefig('default2.png', bbox_inches='tight',format='png') 
+
+experimentName = expName
+
+beams = listFromConfig(Config,experimentName,'beams')
+noises = listFromConfig(Config,experimentName,'noises')
+freqs = listFromConfig(Config,experimentName,'freqs')
+lmax = int(Config.getfloat(experimentName,'lmax'))
+lknee = listFromConfig(Config,experimentName,'lknee')[0]
+alpha = listFromConfig(Config,experimentName,'alpha')[0]
+fsky = Config.getfloat(experimentName,'fsky')
+
+SZProp = SZ_Cluster_Model(clusterCosmology=cc,clusterDict=clusterDict,rms_noises = noises,fwhms=beams,freqs=freqs,lmax=lmax,lknee=lknee,alpha=alpha)
+
+MM = 10**(samples[ids,1])
+zz = samples[ids,0]
+
+Y = SZProp.Y_M(MM,zz) * np.random.normal(1,0.2,nclust)
+
+ML = MM * np.random.normal(1,0.2,nclust)
+
+plt.loglog(ML,Y,'o') 
+plt.savefig('default2.png', bbox_inches='tight',format='png')
+
+#Z = MM
