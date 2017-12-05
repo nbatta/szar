1c99c00606bcb1d3ae786e17edd771e724f88173
Nicholas Battaglia <nbatta@astro.princeton.edu>
2017-12-04 23:28:01 -0500

    beginning to add nemo functions

--- bin/likelihood.py
+++ bin/likelihood.py
@@ -44,7 +44,7 @@ class clusterLike(iniFile,expName,gridname):
         self.cc = ClusterCosmology(fparams,constDict,clTTFixFile=clttfile)
         self.HMF = Halo_MF(cc,mgrid,zgrid)
 
-        def lnprior(self,theta):
+    def lnprior(self,theta):
         a1,a2 = theta
         if  1 < a1 < 5 and  1 < a2 < 2:
             return 0
@@ -67,3 +67,6 @@ class clusterLike(iniFile,expName,gridname):
             return -np.inf
         return lp + self.lnlike(theta, inter)
 
+#Functions from NEMO
+#y0FromLogM500(log10M500, z, tckQFit, tenToA0 = 4.95e-5, B0 = 0.08, Mpivot = 3e14, sigma_int = 0.2)
+#fitQ(parDict, diagnosticsDir, filteredMapsDir)
