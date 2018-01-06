1b4920cf87e10a674270a0699cb730eb33a9911f
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-01-05 21:56:13 -0500

    Reionization = 0 set in cosmology to avoid weird CAMB tau issue

--- scripts/sub_like.sh
+++ scripts/sub_like.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-for i in `seq 0 25`; do  smart_mpi 1 "python bin/run_like.py production_v5 -i $i " -t 12 --include gen4,gen5 ; done
+for i in `seq 0 25`; do  smart_mpi 1 "python bin/run_like.py production_v8 -i $i " -t 12 --include gen4,gen5 ; done
