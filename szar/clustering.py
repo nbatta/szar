added growth factor and fixed bias calculation

szar/clustering.py
@@ -38,9 +38,11 @@ class clustering:
         self.HMF = Halo_MF(self.cc,self.mgrid,self.zgrid)
         self.SZProp = SZ_Cluster_Model(self.cc,self.clusterDict,rms_noises = noise,fwhms=beam,freqs=freq,lknee=lknee,alpha=alpha)
 
+        self.dndm_SZ = self.HMF.dn_dmz_SZ(self.SZProp)
+
     def ntilde(self):
-        dndm_SZ = self.HMF.dn_dmz_SZ(self.SZProp)
-        ans = np.trapz(dndm_SZ,dx=np.diff(self.HMF.M200),axis=0)
+        #dndm_SZ = self.HMF.dn_dmz_SZ(self.SZProp)
+        ans = np.trapz(self.dndm_SZ,dx=np.diff(self.HMF.M200,axis=0),axis=0)
         return ans
 
     def b_eff_z(self):
@@ -50,15 +52,15 @@ class clustering:
         nbar = self.ntilde()
 
         z_arr = self.HMF.zarr
-        dndm_SZ = self.HMF.dn_dmz_SZ(self.SZProp)
+        #dndm_SZ = self.HMF.dn_dmz_SZ(self.SZProp)
         
         R = tinker.radius_from_mass(self.HMF.M200,self.cc.rhoc0om)
-        sigsq = tinker.sigma_sq_integral(R, self.HMF.pk, self.HMF.kh)
+        sig = np.sqrt(tinker.sigma_sq_integral(R, self.HMF.pk, self.HMF.kh))
 
-        blin = tinker.tinker_bias(sigsq,200.)
-        beff = np.trapz(dndm_SZ*blin,dx=np.diff(self.HMF.M200),axis=0) / nbar
+        blin = tinker.tinker_bias(sig,200.)
+        beff = np.trapz(self.dndm_SZ*blin,dx=np.diff(self.HMF.M200,axis=0),axis=0) / nbar
 
-        return beff
+        return beff,blin
         
     def Norm_Sfunc(self,fsky):
         z_arr = self.HMF.zarr
@@ -90,11 +92,13 @@ class clustering:
         return ans
 
     def V_eff(self,mu,fsky):
-        V0 = 1 #FIX
+
+        V0 = self.HMF.dVdz*np.diff(z_arr)*4.*np.pi*fsky #FIX
         nbar = self.ntilde()
         ps = self.ps_bar(mu,fsky)
         npfact = np.multiply(ps,nbar)
         frac = npfact / (1. + npfact)
         ans = np.multiply(frac,V0)
+
         return ans
 

szar/tinker.py
@@ -203,20 +203,20 @@ def dsigma_dkmax_dM(M, z, rho, k, P, comoving=False):
 def tinker_bias_params(Delta):
 
     y = np.log10(Delta)
-    A = 1.0 + 0.24*y*np.exp(-(4/y)**4)
+    A = 1.0 + 0.24*y*np.exp(-(4./y)**4.)
     a = 0.44*y - 0.88
     B = 0.183
     b = 1.5
-    C = 0.019 + 0.107*y + 0.19*np.exp(-(4/y)**4)
+    C = 0.019 + 0.107*y + 0.19*np.exp(-(4./y)**4.)
     c = 2.4
     
     return A,a,B,b,C,c
 
-def tinker_bias(sigsq,Delta):
+def tinker_bias(sig,Delta):
     
     A,a,B,b,C,c = tinker_bias_params(Delta)
     delc = 1.686
-    nu2 = delc**2 / sigsq
-    ans = 1 - A*nu2**a / (nu2**a + delc**a) + B*nu2**b + C*nu2**c
+    nu = delc / sig
+    ans = 1. - A*nu2**a / (nu2**a + delc**a) + B*nu2**b + C*nu2**c
 
     return ans