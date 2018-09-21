b988312bbecd275999a75fd9a5ebef0bba10bfbd
Mathew Madhavacheril <mathewsyriac@gmail.com>
2018-09-21 09:53:07 -0400

    N(M,z) emulator lnlike fix

--- szar/counts.py
+++ szar/counts.py
@@ -212,8 +212,9 @@ def sampleVarianceOverNsquareOverBsquare(cc,kh,pk,z_edges,fsky,lmax=1000):
 class ClusterCosmology(Cosmology):
     
     def __init__(self,paramDict=cosmo.defaultCosmology,constDict=cosmo.defaultConstants,lmax=None,
-                 clTTFixFile=None,skipCls=False,pickling=False,fill_zero=True,dimensionless=True,verbose=True,skipPower=True,skip_growth=True):
-        Cosmology.__init__(self,paramDict,constDict,lmax,clTTFixFile,skipCls,pickling,fill_zero,dimensionless=dimensionless,verbose=verbose,skipPower=skipPower,skip_growth=skip_growth)
+                 clTTFixFile=None,skipCls=False,pickling=False,fill_zero=True,dimensionless=True,
+                 verbose=True,skipPower=True,skip_growth=True,low_acc=False):
+        Cosmology.__init__(self,paramDict,constDict,lmax,clTTFixFile,skipCls,pickling,fill_zero,dimensionless=dimensionless,verbose=verbose,skipPower=skipPower,skip_growth=skip_growth,low_acc=low_acc)
         self.rhoc0om = self.rho_crit0H100*self.om
         
     def E_z(self,z):
